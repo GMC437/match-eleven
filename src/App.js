29dcfbbc04db752cf260bcf84f04393b01330104
Grant <grantmcnally.dev@gmail.com>
2019-09-21 22:45:41 +0100

    used redux in the console

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './js';
 
 function App() {
   return (
---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+import store from "../state/stores";
+import { addArticle } from "../state/actions";
+
+window.store = store;
+window.addArticle = addArticle;
